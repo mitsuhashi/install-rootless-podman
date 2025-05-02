4421028d7aa37b8d163accfea3ddfe7804865f15
Nobutaka Mitsuhashi <mitsuhashi@dbcls.rois.ac.jp>
2025-05-02 11:45:20 +0900

    delete install_rootless_podman_all.yml

--- install_rootless_podman.sh
+++ install_rootless_podman.sh
@@ -1,7 +1,4 @@
-# sudo実行とそうでないtaskを一緒にしている
-#ansible-playbook -v -i localhost, -c local install_rootless_podman_all.yml
-
-# sudo実行のみ
+# sudo実行が必要なtaskのみ
 ansible-playbook -v -i localhost, -c local install_rootless_podman_by_root.yml
 
 # sudo実行でないtaskのみ (先にinstall_rootless_podman_by_root.ymlを実行する必要がある)
